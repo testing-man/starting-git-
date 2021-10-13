260b87c21468ffe4d4c255836c58985cb38ee4cd
testing-man <92450718+testing-man@users.noreply.github.com>
2021-10-13 19:59:28 +0530

    Add files via upload

---
+++ dummy.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class dummy {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine();
+        String str2 = s.nextLine();
+
+        System.out.println(str);
+        System.out.println(str2);
+
+    }
+
+}
